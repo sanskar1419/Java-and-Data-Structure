d92f0c1532827148fb5cd0cf41a8562e67946252
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-05-06 17:51:56 +0530

    Some Pattern question and there solution

---
+++ Patterns/src/CharacterPattern2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CharacterPattern2 {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i=1;
+
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                System.out.print((char)('A'+i-1));
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        s.close();
+    }
+}
