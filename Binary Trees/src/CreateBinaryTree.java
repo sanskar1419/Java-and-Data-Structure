105ee61b5199cabfed4065473d2cd62909facaa9
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-07-26 07:52:51 +0530

    Printing Binary Tree

--- Binary Trees/src/CreateBinaryTree.java
+++ Binary Trees/src/CreateBinaryTree.java
@@ -1,13 +1,23 @@
 public class CreateBinaryTree {
+    public static void printBTRecursively(BinaryNode<Integer> root) {
+        if (root == null)
+            return;
+
+        System.out.print(root.data + " ");
+        printBTRecursively(root.leftNode);
+        printBTRecursively(root.rightNode);
+    }
 
     public static void main(String args[]) {
         BinaryNode<Integer> firstNode = new BinaryNode<Integer>(10);
         BinaryNode<Integer> secondNode = new BinaryNode<Integer>(20);
         BinaryNode<Integer> thirdNode = new BinaryNode<Integer>(30);
-        BinaryNode<Integer> fourthNode = new BinaryNode<Integer>(30);
+        BinaryNode<Integer> fourthNode = new BinaryNode<Integer>(40);
 
         firstNode.leftNode = secondNode;
         firstNode.rightNode = thirdNode;
         thirdNode.leftNode = fourthNode;
+
+        printBTRecursively(firstNode);
     }
 }
