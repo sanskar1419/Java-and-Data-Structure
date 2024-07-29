c1832158be848393beb518b41e569709d74e8bcf
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2024-07-29 11:22:02 +0530

    Level wise input code for binary tree

---
+++ Binary Trees/src/LevelWiseInputOfBinaryTree.java
@@ -0,0 +1,62 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class LevelWiseInputOfBinaryTree {
+    public static BinaryNode<Integer> input() {
+
+        System.out.print("Enter the root element : ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        if (n == -1) {
+            return null;
+        }
+
+        BinaryNode<Integer> root = new BinaryNode<Integer>(n);
+        Queue<BinaryNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            BinaryNode<Integer> currentNode = queue.poll();
+            System.out.print("Enter the left node data of " + currentNode.data + " : ");
+            int left = s.nextInt();
+            if (left != -1) {
+                BinaryNode<Integer> lNode = new BinaryNode<Integer>(left);
+                queue.add(lNode);
+                currentNode.leftNode = lNode;
+            }
+
+            System.out.print("Enter the right node data of " + currentNode.data + " : ");
+            int right = s.nextInt();
+            if (right != -1) {
+                BinaryNode<Integer> rNode = new BinaryNode<Integer>(right);
+                queue.add(rNode);
+                currentNode.rightNode = rNode;
+            }
+        }
+
+        return root;
+    }
+
+    public static void printBTRecursivelyDetailed(
+            BinaryNode<Integer> root) {
+        if (root == null)
+            return;
+
+        System.out.print(root.data + " : ");
+        if (root.leftNode != null)
+            System.out.print("L" + root.leftNode.data + ", ");
+        if (root.rightNode != null)
+            System.out.print("R" + root.rightNode.data);
+
+        System.out.println();
+        printBTRecursivelyDetailed(root.leftNode);
+        printBTRecursivelyDetailed(root.rightNode);
+    }
+
+    public static void main(String args[]) {
+        BinaryNode<Integer> root = input();
+        printBTRecursivelyDetailed(root);
+    }
+}
